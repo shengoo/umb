142d99f1250cc458dfbbc3e78a05ee4ffe0f722c
Qing Sheng <shengoo@hotmail.com>
2014-05-23 00:31:16 +0800

    added files

---
+++ Site/App_Code/MultiLanguage/Data.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Holds data for contstructing a Language Switcher
+/// </summary>
+namespace Goyaweb.MultiLanguage
+{
+    public class Data
+    {
+        public Data()
+        {
+        }
+        public string NativeName { get; set; }
+        public string LanguageCode { get; set; }
+        public string CountryCode { get; set; }
+        public string Url { get; set; }
+    }
+}
