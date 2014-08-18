e33ad6c67ce03b59bdb35ad71901198c2ef038cb
Qing Sheng <shengoo@hotmail.com>
2014-08-19 00:05:07 +0800

    change contact form

--- Site/App_Code/ContactController.cs
+++ Site/App_Code/ContactController.cs
@@ -105,7 +105,6 @@ namespace Koiak.StandardWebsite
         [RegularExpression(@"^(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}$", ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
 
-        [Required]
         [DataType(DataType.MultilineText)] 
         public string Comment { get; set; }
     }
